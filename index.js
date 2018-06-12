bb2fc2b6b20b55ce99bdcff6a8c581af34f674c9
jhoni Faber <jhonifaber@MacBook-Pro-de-jhoni.local>
2018-06-12 13:15:08 +0100

    validate content type header

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ var app = express();
 var bodyParser = require('body-parser')
 var helmet = require('helmet')
 var customerController = require("./src/customers/index.js");
- 
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(helmet())
 
+/** Validate content-type header */
+app.use(function (req, res, next) {
+    var contype = req.headers['content-type'];
+    if (!contype || contype != 'application/json') {
+        return res.sendStatus(406);
+    }
+    next();
+});
+
 app.use("/customers", customerController);
+
+
 
 app.listen(3000, () => {
     console.log("running at port 3000 ");
-    
+
 })
--- src/customers/index.js
+++ src/customers/index.js
@@ -1,4 +1,4 @@
-var express =   require('express');
+var express = require('express');
 var router = express.Router();
 var all = require("./controller/getAllCustomers");
 var id = require("./controller/getCustomerByID");
