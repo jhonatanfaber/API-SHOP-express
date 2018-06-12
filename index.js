ab3ab62d9136b013c1ad62a28e9103771564c95d
jhoni Faber <jhonifaber@MacBook-Pro-de-jhoni.local>
2018-06-12 12:09:04 +0100

    add body parser

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-//var bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
 var customerController = require("./src/customers/index.js");
  
 app.use(express.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.use("/customers", customerController);
 
