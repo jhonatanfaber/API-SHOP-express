2ed77f13ab8aeb548a36e3300e74fb687b268355
jhoni Faber <jhonifaber@MacBook-Pro-de-jhoni.local>
2018-06-12 12:28:58 +0100

    add helmet

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var helmet = require('helmet')
 var customerController = require("./src/customers/index.js");
  
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+app.use(helmet())
 
 app.use("/customers", customerController);
 
