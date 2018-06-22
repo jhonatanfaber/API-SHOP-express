53567c039c2521d3fe4410562368cdce9760dafb
jhoni Faber <jhonifaber@MacBook-Pro-de-jhoni.local>
2018-06-22 13:33:24 +0100

    delete user

---
+++ src/users/controller/deleteUser.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+const rawData = fs.readFileSync("./src/users.json", "utf8")
+var data = JSON.parse(rawData)
+
+module.exports = {
+    deleteUser
+}
+
+function deleteUser(req, res) {
+    let id = req.params.id
+    let userExists = checkIfUserExist(id)
+    if (userExists) {
+        let filteresList = data.filter(user => user.id != id)
+        data.push(filteresList)
+        let newUserJSON = JSON.stringify(filteresList, null, 2);
+        fs.writeFileSync('./src/users.json', newUserJSON);
+        return res.sendStatus(204);
+    }
+    return res.sendStatus(400)
+
+}
+
+function checkIfUserExist(id) {
+    return data.some(user => user.id == id)
+}
--- src/users/index.js
+++ src/users/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router();
 const all = require("./controller/getAllUsers")
 const newUser = require("./controller/createNewUser");
+const remove = require("./controller/deleteUser");
 
 
 router.get("/", all.getAllUsers)
 router.post("/", newUser.createNewUser)
+router.delete("/:id",remove.deleteUser)
 
 module.exports = router
