4e593799a59e741204b9015eeb6631f20fa976b5
Jhoni Faber <jhonatanfaber@gmail.com>
2018-07-18 12:33:59 +0100

    check user doesnt exist in getbyID request

--- src/customers/controller/getCustomerByID.js
+++ src/customers/controller/getCustomerByID.js
@@ -1,17 +1,16 @@
-//var customers = require("./../../customers.json");
-const fs = require("fs")
-const rawdata = fs.readFileSync('./src/customers.json', 'utf8');
-var data = JSON.parse(rawdata)
+const CustomerModel = require("./../model")
 
 module.exports = {
-    getCustomerByID : getCustomerByID
+    getCustomerByID: getCustomerByID
 }
 
-function getCustomerByID (req,res){
-    let id = req.params.id;
-    let user = data.find(user => user.id == id);
-    if(user){
-        return res.status(200).send(user);    
-    }
-    return res.sendStatus(400);
+function getCustomerByID(req, res) {
+    CustomerModel.findOne({ id: req.params.id })
+        .then(user => {
+            if (user) return res.status(200).send(user)
+            return res.sendStatus(400);
+        })
+        .catch(error => {
+            return res.sendStatus(400)
+        })
 }
