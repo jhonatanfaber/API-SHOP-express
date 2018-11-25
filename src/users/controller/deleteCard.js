fc365cc2af8ba43d17c8532290b8f15fef97ed58
Jhoni Faber <jhonatanfaber@gmail.com>
2018-11-25 10:03:28 +0000

    delete card endpoint done

--- src/users/controller/deleteCard.js
+++ src/users/controller/deleteCard.js
@@ -10,13 +10,10 @@ function deleteCard(req, res) {
     
     UserModel.User.findOne({ id: userID })
         .then(user => {
-            // TO FIX: index no es el correcto
-            let index = user.cards.findIndex(card => card.cardId == cardID)
-            console.log(index);
-            
-            // user.cards.splice(index, 1)
-            // user.save()
-            // return res.sendStatus(204);
+            let index = user.cards.findIndex(card => card.cardID == cardID)
+            user.cards.splice(index, 1)
+            user.save()
+            return res.sendStatus(204);
         })
         .catch(() => {
             return res.sendStatus(400)
